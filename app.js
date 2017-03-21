b5636724e5948373e99dc1cfc4976d560b67d975
Chandrashekar <chandrashekar998@gmail.com>
2017-03-21 23:42:49 +0530

    Getting details of place such as state country and latitude and langitude

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const request= require('request');
+const yargs= require('yargs');
 
+const argv= yargs
+  .options({
+    a:{
+      demand:true,
+      alias:'address',
+      describe:'Adress to fetch weather for',
+      string:true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+// console.log(argv);
+// console.log(argv.address);
+
+var encodedAddress=encodeURIComponent(argv.address);
+console.log(encodedAddress);
 request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?address=pes%20university',
+  url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json:true
 },(err,response,body)=>{
-  console.log(JSON.stringify(body));
+  // console.log(JSON.stringify(body, undefined,2));
+   console.log(`Address:${body.results[0].formatted_address}`);
+   console.log(`Latitude:${body.results[0].geometry.location.lat}`);
+   console.log(`Langitude:${body.results[0].geometry.location.lng}`);
+  console.log(`Address:${body.results[0].formatted_address}`);
 });
+
+//encodeURIComponent('pes JD LW');
+//'pes%20JD%20LW'
+//decodeURIComponent('pes%20JD%20LW');
+//'pes JD LW
